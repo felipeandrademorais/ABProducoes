c1cf0ab9be88c587f25743e4c07ee5228b491244
felipeandrademorais <flpiandrade@gmail.com>
2021-12-15 16:09:00 -0300

    Create header

--- src/abproducoes/header.php
+++ src/abproducoes/header.php
@@ -8,3 +8,34 @@
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">    
+            <a class="navbar-brand" href="#">LOGO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse flex-row-reverse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="#" >Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
