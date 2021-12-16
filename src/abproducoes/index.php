04f14b371bd88ff8a373e63c640fb66e55c79503
felipeandrademorais <flpiandrade@gmail.com>
2021-12-16 11:15:42 -0300

    Second Section

--- src/abproducoes/index.php
+++ src/abproducoes/index.php
@@ -2,6 +2,7 @@
     get_header();
 
     get_template_part('template-parts/index/section-1');
+    get_template_part('template-parts/index/section-2');
     
     get_footer();
 ?>
--- src/abproducoes/template-parts/index/section-2.php
+++ src/abproducoes/template-parts/index/section-2.php
@@ -1,8 +1,55 @@
+<?php
+    $args = array(
+        'post_status'    => 'publish',
+        'post_type'      => 'artistas',
+    );
+
+    $the_content = new WP_Query($args);
+?>
+
 <section>
     <div class="container">
         <h3>Nossos Artistas</h3>
         <div class="artistas">
-            
+            <?php if ($the_content->have_posts()) : ?>
+                <?php while ($the_content->have_posts()) : $the_content->the_post(); ?>
+                    <!-- Variables -->
+                    <?php 
+                        $image = get_field('imagem');
+                        $nome_field = get_field('nome');
+                        $nome = strval($nome_field);
+                    ?>
+
+                    <!-- Buttom -->
+                    <button 
+                        type="button" 
+                        data-toggle="modal" 
+                        data-target="#<?php echo preg_replace('/\s+/', '', $nome); ?>"  
+                        style="background-image: url('<?php echo esc_url($image); ?>')"
+                    ></button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="<?php echo preg_replace('/\s+/', '', $nome); ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header" style="background-image: url('<?php echo esc_url($image); ?>')">
+                                
+                            </div>
+                            <div class="modal-body">
+                                <?php echo get_field('descricao'); ?>
+                            </div>
+                            <div class="modal-footer">
+                                
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+            <?php else : ?>
+                <div><?php _e('Nenhum artista cadastrado até o momento'); ?></div>
+            <?php endif; ?>
         </div>
     </div>
 </section>
