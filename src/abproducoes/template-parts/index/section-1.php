0460e20cc0810b431c93d68f41511638d5d5530b
felipeandrademorais <flpiandrade@gmail.com>
2021-12-21 17:15:17 -0300

    Ajustes

--- src/abproducoes/template-parts/index/section-1.php
+++ src/abproducoes/template-parts/index/section-1.php
@@ -3,7 +3,7 @@
         <div class="combo_text">
             <h1><?php echo get_theme_mod('set_section_1_titulo');?></h1>
             <p><?php echo get_theme_mod('set_section_1_subtitulo');?></p>
-            <a class="btn btn-primary" href="#" >Contato</a>
+            <a class="btn btn-primary" href="#contato" >Contato</a>
         </div>
     </div>
 </section>
