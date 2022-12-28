f32610a738ac45491221cdbeb5c65034cead5020
felipeandrademorais <flpiandrade@gmail.com>
2022-12-28 11:22:16 -0300

    Ajustes produção

--- footer.php
+++ footer.php
@@ -1,29 +1,29 @@
-    <?php 
-        $args = array(
-            'post_status'    => 'publish',
-            'post_type'      => 'logos_rodape',
-        );
+    <?php
+    $args = array(
+        'post_status'    => 'publish',
+        'post_type'      => 'logos_rodape',
+    );
 
-        $the_eventos = new WP_Query($args);
+    $the_eventos = new WP_Query($args);
     ?>
 
     <div class="footer destaque">
         <div class="menu_bar">
             <ul class="nav_container">
                 <?php if ($the_eventos->have_posts()) : ?>
-                    <?php 
-                        while ($the_eventos->have_posts()) : $the_eventos->the_post();
-                            $imagens = get_field('imagem'); 
-                            foreach($imagens as $image):
+                    <?php
+                    while ($the_eventos->have_posts()) : $the_eventos->the_post();
+                        $imagens = get_field('imagem');
+                        foreach ($imagens as $image) :
                     ?>
-                                <li class="nav-item">
-                                    <a class="navbar-brand" href="#"><img src="<?php echo $image['full_image_url']?>" alt="<?php echo $image['title']; ?>"></a>
-                                </li>
-                    <?php 
-                            endforeach; 
-                        endwhile; 
+                            <li class="nav-item">
+                                <a class="navbar-brand" href="#"><img src="<?php echo $image['full_image_url'] ?>" alt="<?php echo $image['title']; ?>"></a>
+                            </li>
+                    <?php
+                        endforeach;
+                    endwhile;
                     ?>
-                        <?php wp_reset_postdata(); ?>                
+                    <?php wp_reset_postdata(); ?>
                 <?php endif; ?>
             </ul>
         </div>
@@ -43,37 +43,25 @@
                     <a class="nav-link" href="#section-4">Depoimentos</a>
                 </li>
                 <li class="nav-item">
-                        <a class="nav-link" href="#section-6">Eventos</a>
-                    </li>
+                    <a class="nav-link" href="#section-6">Eventos</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#section-5">Parceiros</a>
                 </li>
             </ul>
         </div>
         <div class="social">
-            
-            <a 
-                target="_blank" 
-                class="<?php echo empty(get_theme_mod('set_facebook')) ? 'd-none' : 'social_item';?>" 
-                href="<?php echo get_theme_mod('set_facebook');?>"
-            >
+
+            <a target="_blank" class="<?php echo empty(get_theme_mod('set_facebook')) ? 'd-none' : 'social_item'; ?>" href="<?php echo get_theme_mod('set_facebook'); ?>">
                 <span class="iconify" data-icon="entypo-social:facebook-with-circle"></span>
             </a>
-            <a 
-                target="_blank"
-                class="<?php echo empty(get_theme_mod('set_instagram')) ? 'd-none' : 'social_item';?>" 
-                href="<?php echo get_theme_mod('set_instagram');?>"
-            >
+            <a target="_blank" class="<?php echo empty(get_theme_mod('set_instagram')) ? 'd-none' : 'social_item'; ?>" href="<?php echo get_theme_mod('set_instagram'); ?>">
                 <span class="iconify" data-icon="entypo-social:instagram-with-circle"></span>
             </a>
-            <a 
-                target="_blank"
-                class="<?php echo empty(get_theme_mod('set_youtube')) ? 'd-none' : 'social_item';?>"  
-                href="<?php echo get_theme_mod('set_youtube');?>"
-            >
+            <a target="_blank" class="<?php echo empty(get_theme_mod('set_youtube')) ? 'd-none' : 'social_item'; ?>" href="<?php echo get_theme_mod('set_youtube'); ?>">
                 <span class="iconify" data-icon="entypo-social:youtube-with-circle"></span>
             </a>
-            
+
         </div>
         <div class="privacy_terms">
             <small><?php echo get_theme_mod('set_privacity_text'); ?></small>
@@ -82,4 +70,5 @@
     </div>
     <?php wp_footer(); ?>
     </body>
-</html>
+
+    </html>
