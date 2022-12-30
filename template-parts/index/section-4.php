c92753824d88ed0cf3139339b6f711f33192db97
felipeandrademorais <flpiandrade@gmail.com>
2022-12-30 09:49:07 -0300

    Ajustes na listagem de depoimentos

--- template-parts/index/section-4.php
+++ template-parts/index/section-4.php
@@ -1,38 +1,36 @@
-<?php 
-     $args = array(
+<?php
+$args = array(
     'post_status'    => 'publish',
     'post_type'      => 'depoimentos',
     'posts_per_page'  => -1,
-    );
+);
 
-    $the_depoimentos = new WP_Query($args);
+$the_depoimentos = new WP_Query($args);
 ?>
 
 
-<section class="depoimentos back-ligth" id="section-4">
-    <div class="container">
-        <div class="depoimentos_slider">
-        <?php if ($the_depoimentos->have_posts()) : ?>
-            <?php while ($the_depoimentos->have_posts()) : $the_depoimentos->the_post(); ?>
-                <div class="carousel-depoimento-item"  style="background-image: url('<?php echo get_field('fundo'); ?>');">
-                    <div class="carousel-depoimento-item-cover">
-                        <div class="text">
-                            <p><?php echo get_field('texto') ?></p>
-                        </div>
-                        <div class="img">
-                            <img src="<?php echo get_field('imagem'); ?>" alt="imagem_depoimento" />
-                        </div>
-                        <div class="name">
-                            <h3><?php echo get_field('nome') ?></h3>
-                            <p><?php  echo get_field('cargo') ?></p>
+<?php if ($the_depoimentos->have_posts()) : ?>
+    <section class="depoimentos back-ligth" id="section-4">
+        <div class="container">
+            <div class="depoimentos_slider">
+                <?php while ($the_depoimentos->have_posts()) : $the_depoimentos->the_post(); ?>
+                    <div class="carousel-depoimento-item" style="background-image: url('<?php echo get_field('fundo'); ?>');">
+                        <div class="carousel-depoimento-item-cover">
+                            <div class="text">
+                                <p><?php echo get_field('texto') ?></p>
+                            </div>
+                            <div class="img">
+                                <img src="<?php echo get_field('imagem'); ?>" alt="imagem_depoimento" />
+                            </div>
+                            <div class="name">
+                                <h3><?php echo get_field('nome') ?></h3>
+                                <p><?php echo get_field('cargo') ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
-        <?php else : ?>
-            <div><?php _e('Nenhum depoimento cadastrado'); ?></div>
-        <?php endif; ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
